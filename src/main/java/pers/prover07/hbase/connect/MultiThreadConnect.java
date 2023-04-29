98cf2b20ecfca2e373429533f3ca1dd2814b0ccb
Dreamer-07 <2391105059@qq.com>
2023-04-29 10:25:26 +0800

    add hbase api

--- src/main/java/pers/prover07/hbase/connect/MultiThreadConnect.java
+++ src/main/java/pers/prover07/hbase/connect/MultiThreadConnect.java
@@ -1,8 +1,10 @@
 package pers.prover07.hbase.connect;
 
-import org.apache.hadoop.hbase.NamespaceDescriptor;
-import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.ColumnValueFilter;
+import org.apache.hadoop.hbase.filter.FilterList;
+import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -131,8 +133,179 @@ public class MultiThreadConnect {
      */
     @Test
     public void deleteTableColumn() {
+        try (Admin admin = connection.getAdmin()) {
 
+            TableName tableName = TableName.valueOf("bigData", "test1");
+            admin.deleteColumnFamily(tableName, "test2".getBytes());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    /**
+     * 插入数据
+     */
+    @Test
+    public void insertData() {
+        // 获取 table 连接
+        try (Table table = connection.getTable(TableName.valueOf("bigData", "test1"))) {
+            // 创建写入数据对象(RowKey)
+            Put put = new Put("1001".getBytes());
+            // 设置列族，列名，数据
+            put.addColumn("info".getBytes(), "name".getBytes(), "prover2".getBytes());
+            put.addColumn("options".getBytes(), "disabled".getBytes(), "0".getBytes());
+
+            // 插入数据
+            table.put(put);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 获取数据(读取对应一行的某一列)
+     */
+    @Test
+    public void getData() {
+        // 获取表信息
+        try (Table table = connection.getTable(TableName.valueOf("bigData", "test1"))) {
+            // 创建 get 对象(RowKey)
+            Get get = new Get("1001".getBytes());
+
+            // 配置要读取的列
+            get.addColumn("info".getBytes(), "name".getBytes());
+            get.addColumn("options".getBytes(), "disabled".getBytes());
+
+            // 设置要读取的数据版本
+            get.readAllVersions();
+
+            // 获取数据
+            Result result = table.get(get);
+
+            // 处理数据
+            Cell[] cells = result.rawCells();
+            for (Cell cell : cells) {
+                System.out.println(new String(CellUtil.cloneValue(cell)));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 获取数据(读取多行)
+     */
+    @Test
+    public void scanRows() {
+        try (// 获取 table
+             Table table = connection.getTable(TableName.valueOf("bigData", "test1"));
+        ) {
+
+            // 创建 scan
+            Scan scan = new Scan();
+            // 配置 scan(不配置会扫描所有数据)
+            // 默认包含
+            scan.withStartRow("1001".getBytes());
+            // 默认包含
+            scan.withStopRow("1010".getBytes());
+
+            // 读取数据
+            ResultScanner results = table.getScanner(scan);
+            for (Result result : results) {
+                Cell[] cells = result.rawCells();
+
+                for (Cell cell : cells) {
+                    System.out.print(new String(CellUtil.cloneRow(cell)) + "-" +
+                            new String(CellUtil.cloneFamily(cell)) + "-" +
+                            new String(CellUtil.cloneQualifier(cell)) + "-" +
+                            new String(CellUtil.cloneValue(cell)) + "\t"
+                    );
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 获取数据(带过滤条件)
+     */
+    @Test
+    public void filterScan() {
+        try (// 获取 table
+             Table table = connection.getTable(TableName.valueOf("bigData", "test1"))
+        ) {
+            // 创建 scan
+            Scan scan = new Scan();
+            // 配置 scan(不配置会扫描所有数据)
+            // 默认包含
+            scan.withStartRow("1001".getBytes());
+            // 默认包含
+            scan.withStopRow("1010".getBytes());
+
+            // 配置过滤条件
+            FilterList filterList = new FilterList();
+            // 创建过滤器
+            // 1. ColumnValueFilter - 只显示配置的列信息
+            // ColumnValueFilter filter = new ColumnValueFilter("info".getBytes(), "name".getBytes(), CompareOperator.EQUAL, "prover2".getBytes());
+
+            // 2. SingleColumnValueFilter - 会显示符合条件(包括没有该列)的所有列信息
+            // 例如 1002 没有 info.name 这个列的数据，那么也会显示
+            // 但 1003 有 info.name 的数据，但是不符合条件, 就不会显示
+            // 且除了 name 列，符合条件的行的所有列都会显示出来
+            SingleColumnValueFilter filter = new SingleColumnValueFilter("info".getBytes(), "name".getBytes(), CompareOperator.EQUAL, "prover".getBytes());
+
+            // 添加到过滤器链中
+            filterList.addFilter(filter);
+            scan.setFilter(filterList);
+
+            // 读取数据
+            ResultScanner results = table.getScanner(scan);
+            for (Result result : results) {
+                Cell[] cells = result.rawCells();
+
+                for (Cell cell : cells) {
+                    System.out.print(new String(CellUtil.cloneRow(cell)) + "-" +
+                            new String(CellUtil.cloneFamily(cell)) + "-" +
+                            new String(CellUtil.cloneQualifier(cell)) + "-" +
+                            new String(CellUtil.cloneValue(cell)) + "\t"
+                    );
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 删除数据
+     */
+    @Test
+    public void deleteData() {
+        try (// 获取 table
+             Table table = connection.getTable(TableName.valueOf("bigData", "test1"))
+        ) {
+            // 创建 delete 对象(RowKey)
+            Delete delete = new Delete("1001".getBytes());
+
+            // 删除单个版本(回到上个版本)
+            // delete.addColumn("info".getBytes(), "name".getBytes());
+
+            // 删除所有版本
+            delete.addColumns("info".getBytes(), "name".getBytes());
+
+            // 删除列族
+            // delete.addFamily("info".getBytes());
+
+            table.delete(delete);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public static void closeConnection() {
         try {
